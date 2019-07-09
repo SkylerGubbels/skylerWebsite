fa2fce1fd66385e585ea2e77279525cc824da62e
Skyler <skylermgubbels@gmail.com>
2019-07-09 15:54:00 -0400

    Background color added

--- src/components/summary.jsx
+++ src/components/summary.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import horizon from "../resources/horizon.jpg"
+import { getSummaryText } from "../resources/englishText";
 import "animate.css"
 
 class Summary extends Component {
@@ -8,8 +9,13 @@ class Summary extends Component {
         return ( 
         <div>
             <img className="backgroundImage" src={horizon} alt=""></img>
-            <h1 className="animated bounceInRight"><span>Skyler Gubbels</span></h1>
-            <h2 className="animated bounceInRight"><span>Software Developer</span></h2>
+            <span className="">Photo taken by: https://www.instagram.com/shangerdanger/</span>
+            <div className="imageText">
+            <h1 className="animated bounceInRight summaryTitle"><span className="imageSpan">Skyler Gubbels</span></h1>
+            <h2 className="animated bounceInRight summarySubtitle"><span className="imageSpan">Software Developer</span></h2>
+            <span className="summaryText animated bounceInRight">{getSummaryText().summary}</span>
+            </div>
+            <i class="fa fa-angle-down" aria-hidden="true"></i>
         </div> );
     }
 }
