0d03c8807275321f647f70eea413331b390f2a0d
Skyler <skylermgubbels@gmail.com>
2019-07-14 17:14:25 -0400

    Navbar links underline when hovered. All navbar text changes color when navbar opacity changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -26,25 +26,27 @@ class NavBar extends Component {
       let { className, backgroundColor,language, onLanguageChange } = this.props
       if(!className) className = "navbar navbar-expand navbar-light bg-light navbar-static-top navbar-background";
       if(!backgroundColor) backgroundColor = "rgb(255,255,255)";
-      const textColor = (this.props.textColor) || "rgb(255,255,255)";
+      const textColor = (this.props.textColor) || "rgb(0,0,0)"; // If no value for textcolor then we make it black
       
       return(
         <nav className={className} style={{"backgroundColor":backgroundColor}}>
         <img src={skylerImage} className="m-2" alt="" width="50"/>
+        
         <Link className="navbar-brand" to="/about#introduction"> 
-          <span style={{"color":textColor, "font-weight":"bold"}}>Skyler Gubbels</span>
+          <span className="navText" style={{"color":textColor, "font-weight":"bold"}}>Skyler Gubbels</span>
           <br/>
           <span style={{"color":textColor}}>Software Developer</span> 
         </Link>
+        
         <div className="collapse navbar-collapse navbar-nav flex-row-reverse" id="navbar">
           <div className="navbar-nav"></div>
             {(language === "french") && <button className="nav-item nav-link" onClick={() => onLanguageChange("english")}>English</button>}
             {(language === "english") && <button className="nav-item nav-link" onClick={() => onLanguageChange("french")}>Français</button>}
-            <NavLink className="nav-item nav-link" to="/contact"> Contact</NavLink>
-            <NavLink className="nav-item nav-link" to="/projects"> Projects </NavLink>
+            <NavLink className="nav-item nav-link navText" to="/contact"> <span style={{"color":textColor}}>Contact</span></NavLink>
+            <NavLink className="nav-item nav-link navText" to="/projects"> <span style={{"color":textColor}}>Projects</span> </NavLink>
           <li className="nav-item dropdown">
-                <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                About
+                <Link className="nav-link dropdown-toggle navText" to="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style={{"color":textColor}}>
+                  <span style={{"color":textColor}}>About</span>
                 </Link>
                 <div className="dropdown-menu" aria-labelledby="navbarDropdown">
                   { getDropDownItems().links.map((item) => this.createDropdown(item))}
