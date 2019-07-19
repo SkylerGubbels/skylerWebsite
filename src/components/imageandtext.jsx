fc7d834ff11c6c5b441cac53f0ca9c329fbb8fb4
Skyler <skylermgubbels@gmail.com>
2019-07-19 16:29:40 -0400

    Adding component description. Modified imageandtext.jsx to fit better on mobile screens

--- src/components/imageandtext.jsx
+++ src/components/imageandtext.jsx
@@ -13,7 +13,7 @@ const ImageAndText = (props) => {
                 <img className={imageClass} src={image} width={width} height={height} alt=""/>
             </div>
             <div className="col-sm-0 col-xs-1"></div>
-            <div className="col-8">
+            <div className="col-8 col-xs-7">
                 <p className="categoryTitle summaryTitle whiteText">{title}</p>
                 <p className={textClass}>{text}</p>
             </div>
